e01cc2c31e0432346c8565ac44f968318e7766a9
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-12 16:01:05 -0600

    renamed Display

--- src/edge/pixi/components/DisplaySprite.hx
+++ src/edge/pixi/components/DisplaySprite.hx
@@ -4,10 +4,10 @@ import edge.IComponent;
 import pixi.core.sprites.Sprite;
 import pixi.core.textures.Texture;
 
-class Display implements IComponent {
+class DisplaySprite implements IComponent {
   var sprite : Sprite;
 
 	public static function fromImagePath(path : String) {
-		return new Display(new Sprite(Texture.fromImage(path)));
+		return new DisplaySprite(new Sprite(Texture.fromImage(path)));
 	}
 }
